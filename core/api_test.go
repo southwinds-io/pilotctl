bf824c570399bd2614efec2b6692bf3629f0cc3a
swskipper <skipper@southwinds.io>
2022-11-08 16:48:46 +0000

    update test

--- core/api_test.go
+++ core/api_test.go
@@ -3,6 +3,7 @@ package core
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"testing"
 )
 
@@ -14,18 +15,33 @@ func TestSubmitMetrics(t *testing.T) {
 	os.Setenv("PILOT_CTL_ILINK_USER", "admin")
 	os.Setenv("PILOT_CTL_ILINK_PWD", "0n1x")
 	os.Setenv("PILOT_CTL_ILINK_INSECURE_SKIP_VERIFY", "true")
-	os.Setenv("PILOT_CTL_TELEM_CONN", "test:pravega-conn")
-	os.Setenv("PRAVEGA_CONN_HOST", "localhost:9090")
-	os.Setenv("PRAVEGA_CONN_SCOPE", "host")
-	os.Setenv("PRAVEGA_CONN_STREAM", "metrics")
+	os.Setenv("PILOT_CTL_TELEM_CONN", "test:mongo-metrics")
+
+	// pravega connector  settings
+	// os.Setenv("PRAVEGA_CONN_HOST", "localhost:9090")
+	// os.Setenv("PRAVEGA_CONN_SCOPE", "host")
+	// os.Setenv("PRAVEGA_CONN_STREAM", "metrics")
+
+	// mongo connector settings
+	os.Setenv("MONGO_CONN_CONNECTION_STRING", "mongodb://root:example@localhost:27017")
+	os.Setenv("MONGO_CONN_DATABASE", "metrics")
+	os.Setenv("MONGO_CONN_METRICS_COLLECTION", "host")
+
 	api, err := NewAPI(NewConf())
 	if err != nil {
 		t.Fatalf(err.Error())
 	}
-	c, err := os.ReadFile("protobuf.pb")
-	if err != nil {
-		t.Fatalf(err.Error())
+	entries, _ := os.ReadDir("test-data")
+	for _, entry := range entries {
+		if entry.IsDir() {
+			continue
+		}
+		abs, _ := filepath.Abs(filepath.Join("test-data", entry.Name()))
+		content, err := os.ReadFile(abs)
+		if err != nil {
+			t.Fatalf(err.Error())
+		}
+		r := api.SubmitMetrics("test", content)
+		fmt.Println(r.Error)
 	}
-	r := api.SubmitMetrics("test", c)
-	fmt.Println(r.Error)
 }
